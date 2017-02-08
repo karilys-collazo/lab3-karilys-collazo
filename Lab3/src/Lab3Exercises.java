94c09c7689aae416b3e116266c793f420611876f
4015c5 <4015c5@mamey.ece.uprm.edu>
2017-02-08 12:15:25 -0400

    Name of the frame has been changed. Position and size of the frame has been changed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,20 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Karilys Collazo");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	    int center_a = (int) ((dimension.getWidth() - myFrame.getWidth()) / 2);
+	    int center_b = (int) ((dimension.getHeight() - myFrame.getHeight()) / 2);
+	    myFrame.setLocation(center_a, center_b);
+		
 	}
 }
